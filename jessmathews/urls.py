9d319f658362a71c9d5d24cc99d8efb57c866940
Thomas Cuthbert <tcuthbert90@gmail.com>
2015-04-25 18:47:00 +0800

    added basic templates and url

---
+++ jessmathews/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.views.generic import TemplateView 
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'jessmathews.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(
+        template_name="gallery/homepage.html"
+    ))
+]
