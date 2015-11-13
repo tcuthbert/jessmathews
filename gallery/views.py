b96c8f8a1115b5a9776a592a1240be09a83961d0
Thomas Cuthbert <tcuthbert90@gmail.com>
2015-11-13 19:43:42 +0800

    updated

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from zinnia.models.entry import Entry
 
 # Create your views here.
+
+
+class HomePage(ListView):
+    model = Entry
+    template_name = "gallery/homepage.html"
--- jessmathews/urls.py
+++ jessmathews/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView 
 from django.conf import settings
 from django.conf.urls.static import static
+from gallery.views import HomePage
 
 urlpatterns = [
     # Examples:
@@ -16,7 +17,8 @@ urlpatterns = [
     url(r'^contact/', TemplateView.as_view(
         template_name="gallery/contact.html"
     )),
-    url(r'^$', TemplateView.as_view(
-        template_name="gallery/homepage.html"
-    )),
+    url(r'^$', HomePage.as_view()),
+    #url(r'^$', TemplateView.as_view(
+        #template_name="gallery/homepage.html"
+    #)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
